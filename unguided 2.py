d67fe2397d8a7df706cf5037e04accf2e9f0676a
Kezia Trifena <93313412+keziatrifena@users.noreply.github.com>
2022-09-05 19:46:55 +0700

    Update unguided 2.py

--- unguided 2.py
+++ unguided 2.py
@@ -8,7 +8,7 @@ def fibo_rek(n):
             return fibo_rek(n-1) + fibo_rek(n-2)
 
 
-for i in range(1,12):
+for i in range(1,101):
     start1 = timeit.default_timer()
     end1 = timeit.default_timer()
     print("Fibonacci rekursif: ", i, "dengan waktu: ", end1-start1, "second")
@@ -24,7 +24,7 @@ def fibonacci_iteraktif(n):
         b = x + b
     return a
 
-for i in range (1, 11):
+for i in range (1, 101):
     start = timeit.default_timer()
     end = timeit.default_timer()
     print("Bilangan fibonacci ", i, "adalah", fibonacci_iteraktif(i), "selama", start-end, "detik.")
